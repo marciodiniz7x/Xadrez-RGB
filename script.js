4c731d6d9edcc230d8020149379ca27d6c910e8e
Márcio Diniz <marcio.sccp10@gmail.com>
2023-03-29 14:13:42 -0300

    Updates

--- script.js
+++ script.js
@@ -47,94 +47,98 @@ for (let i = 0; i < 8; i++) {
 const square = document.querySelector('.square');
 const squares = document.querySelectorAll('.square');
 
-const botoes = document.querySelector('button');
+const buttons = document.querySelectorAll('.button');
 
-bloody.addEventListener('click', function() {
+// Percorre cada botão e adiciona um listener click
+buttons.forEach(function(button) {
+    button.addEventListener('click', () => {
 
-    body.style.background = 'url(img/bg01-verm.png)';
-    totalArea.style.boxShadow = 'var(--shadow-bloody)';
-    totalArea.style.background = 'var(--bg-bloody)';
-    squares.forEach(function(square) {
-        square.style.opacity = '0';
-        setTimeout(() => {
-            square.style.background = 'var(--cor-bloody)';
-            square.style.opacity = '1';
-        }, 200);
-    });
-    
-});
+        // Remove a classe active de todos os botões
+        buttons.forEach(function(button) {
+            button.classList.remove('active');
+        });
 
-evil.addEventListener('click', function() {
+        // Adiciona a classe active no botão clicado
+        button.classList.add('active');
 
-    body.style.background = 'url(img/bg01-rox.png)';
-    totalArea.style.boxShadow = 'var(--shadow-evil)';
-    totalArea.style.background = 'var(--bg-evil)';
-    squares.forEach(function(square) {
-        square.style.opacity = '0';
-        setTimeout(() => {
-            square.style.background = 'var(--cor-evil)';
-            square.style.opacity = '1';
-        }, 200);
-    });
+        // Define os estilos com base no botão clicado
+        switch (button.classList[1]) {
+            case 'bloody':
+                body.style.background = 'url(img/bg01-verm.png)';
+                totalArea.style.boxShadow = 'var(--shadow-bloody)';
+                totalArea.style.background = 'var(--bg-bloody)';
+                squares.forEach(function(square) {
+                    square.style.opacity = '0';
+                    setTimeout(() => {
+                    square.style.background = 'var(--cor-bloody)';
+                    square.style.opacity = '1';
+                    }, 200);
+                });
+                break;
+            case 'evil':
+                body.style.background = 'url(img/bg01-rox.png)';
+                totalArea.style.boxShadow = 'var(--shadow-evil)';
+                totalArea.style.background = 'var(--bg-evil)';
+                squares.forEach(function(square) {
+                    square.style.opacity = '0';
+                    setTimeout(() => {
+                    square.style.background = 'var(--cor-evil)';
+                    square.style.opacity = '1';
+                    }, 200);
+                });
+                break;
+            case 'fairy':
+                body.style.background = 'url(img/bg01-verd.png)';
+                totalArea.style.boxShadow = 'var(--shadow-fairy)';
+                totalArea.style.background = 'var(--bg-fairy)';
+                squares.forEach(function(square) {
+                    square.style.opacity = '0';
+                    setTimeout(() => {
+                    square.style.background = 'var(--cor-fairy)';
+                    square.style.opacity = '1';
+                    }, 200);
+                });
+                break;
+            case 'bee':
+                body.style.background = 'url(img/bg01-amar.png)';
+                totalArea.style.boxShadow = 'var(--shadow-bee)';
+                totalArea.style.background = 'var(--bg-bee)';
+                squares.forEach(function(square) {
+                    square.style.opacity = '0';
+                    setTimeout(() => {
+                    square.style.background = 'var(--cor-bee)';
+                    square.style.opacity = '1';
+                    }, 200);
+                });
+                break;
+            case 'dark':
+                body.style.background = 'url(img/bg01-dark.png)';
+                totalArea.style.boxShadow = 'var(--shadow-dark)';
+                totalArea.style.background = 'var(--bg-dark)';
+                squares.forEach(function(square) {
+                    square.style.opacity = '0';
+                    setTimeout(() => {
+                    square.style.background = 'var(--cor-dark)';
+                    square.style.opacity = '1';
+                    }, 200);
+                });
+                break;
+            case 'stock':
+                body.style.background = 'url(img/bg01.jpg)';
+                totalArea.style.boxShadow = 'var(--shadow-stock)';
+                totalArea.style.background = 'var(--bg-stock)';
+                squares.forEach(function(square) {
+                    square.style.opacity = '0';
+                    setTimeout(() => {
+                    square.style.background = 'var(--cor-stock)';
+                    square.style.opacity = '1';
+                    }, 200);
+                });
+                break;
+                default:
+                break;
+        }
+    })
+})
 
-});
 
-fairy.addEventListener('click', function() {
-
-    body.style.background = 'url(img/bg01-verd.png)';
-    totalArea.style.boxShadow = 'var(--shadow-fairy)';
-    totalArea.style.background = 'var(--bg-fairy)';
-    squares.forEach(function(square) {
-        square.style.opacity = '0';
-        setTimeout(() => {
-            square.style.background = 'var(--cor-fairy)';
-            square.style.opacity = '1';
-        }, 200);
-    });
-
-});
-
-bee.addEventListener('click', function() {
-
-    body.style.background = 'url(img/bg01-amar.png)';
-    totalArea.style.boxShadow = 'var(--shadow-bee)';
-    totalArea.style.background = 'var(--bg-bee)';
-    squares.forEach(function(square) {
-        square.style.opacity = '0';
-        setTimeout(() => {
-            square.style.background = 'var(--cor-bee)';
-            square.style.opacity = '1';
-        }, 200);
-    });
-
-});
-
-dark.addEventListener('click', function() {
-
-    body.style.background = 'url(img/bg01-dark.png)';
-    totalArea.style.boxShadow = 'var(--shadow-dark)';
-    totalArea.style.background = 'var(--bg-dark)';
-    squares.forEach(function(square) {
-        square.style.opacity = '0';
-        setTimeout(() => {
-            square.style.background = 'var(--cor-dark)';
-            square.style.opacity = '1';
-        }, 200);
-    });
-
-});
-
-stock.addEventListener('click', function() {
-
-    body.style.background = 'url(img/bg01.jpg)';
-    totalArea.style.boxShadow = 'var(--shadow-stock)';
-    totalArea.style.background = 'var(--bg-stock)';
-    squares.forEach(function(square) {
-        square.style.opacity = '0';
-        setTimeout(() => {
-            square.style.background = 'var(--cor-stock)';
-            square.style.opacity = '1';
-        }, 200);
-    });
-
-});
