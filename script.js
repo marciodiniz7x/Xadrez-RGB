f9af0fddb0594ca6a0723e71b38e2f3dacc219e7
Márcio Diniz <marcio.sccp10@gmail.com>
2023-04-14 11:58:40 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const bloody = document.querySelector('.bloody');
+const evil = document.querySelector('.evil');
+const fairy = document.querySelector('.fairy');
+const bee = document.querySelector('.bee');
+const dark = document.querySelector('.dark');
+const stock = document.querySelector('.stock');
+
+const body = document.querySelector('body');
+const totalArea = document.querySelector('.total-area');
+
+// Seleciona tabuleiro
+const board = document.querySelector('.board'); 
+
+// Seleciona as coordenadas do tabuleiro
+const coord = document.querySelector('.coord');
