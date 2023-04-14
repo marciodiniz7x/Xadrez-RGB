4b35b1e5318e8643319f9c21565035237acbb31f
Márcio Diniz <marcio.sccp10@gmail.com>
2023-04-14 12:58:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ const board = document.querySelector('.board');
 
 // Seleciona as coordenadas do tabuleiro
 const coord = document.querySelector('.coord');
+
+// Loop para adicionar linhas até que tenham 8
+for (let i = 0; i < 8; i++) {
+
+    const row = document.createElement('tr');
+    for (let j = 0; j < 8; j++) {
+        const cell = document.createElement('td');
+        // Verifica se a  soma dos índices da linha e coluna é par
+        if ((i+j) % 2 === 0) {
+            cell.classList.add('square-black'); 
+        } else {
+            cell.classList.add('square');
+        }
+        row.appendChild(cell);
+    }
+    board.appendChild(row);
+
+}
+
+// Seletores dos elementos criados via loop
+const square = document.querySelector('.square');
+const squares = document.querySelectorAll('.square');
+const buttons = document.querySelectorAll('.button');
